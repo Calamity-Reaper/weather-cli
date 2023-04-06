d3ef2f918f39b57e8beff98f5bf213a7bc93f8ee
Illia Kokhanovskiy <ilya.kohanovskiy2004@gmail.com>
2023-04-06 16:46:26 +0300

    Added log service

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js'
+import {printHelp} from "./services/log.service.js";
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
-        //Print help
+        printHelp();
     }
     if (args.s) {
         //Save city
