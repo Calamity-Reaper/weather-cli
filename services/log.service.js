9bc32d9d2a08bd616b391a57b2d972b06361721e
Illia Kokhanovskiy <ilya.kohanovskiy2004@gmail.com>
2023-04-06 16:56:44 +0300

    Added log service

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from 'chalk';
+import dedent from "dedent-js";
+
+const printError = (err) => {
+    console.log(chalk.bgRed(`ERROR ${err}`));
+};
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen(`SUCCESS ${message}`));
+};
+
+const printHelp = () => {
+    console.log(
+      dedent`${chalk.bgCyan.black(` HELP `)}
+        Without params - print weather
+        -s [CITY] - set your city
+        -h - print help
+        -t [API_KEY] - set api key
+      `
+    );
+}
+
+export {printError, printSuccess, printHelp}
