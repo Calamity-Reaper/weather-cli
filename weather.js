40be4c52f9dd70cd332be6b548f7264e588cd390
Illia Kokhanovskiy <ilya.kohanovskiy2004@gmail.com>
2023-04-06 17:34:01 +0300

    Storage service and saving api token to file

--- weather.js
+++ weather.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js'
-import {printHelp} from "./services/log.service.js";
+import {printHelp, printSuccess, printError} from "./services/log.service.js";
+import {saveKeyValue} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Token is saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
@@ -10,7 +22,7 @@ const initCLI = () => {
         //Save city
     }
     if (args.t) {
-        //Save token
+        return saveToken(args.t);
     }
     //Print weather
 };
